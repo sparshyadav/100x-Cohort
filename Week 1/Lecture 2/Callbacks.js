188a3acffe9f023fda4dd2c62de30485f18cbabe
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-12 11:45:31 +0530

    Creating a Callback Function to find Sum of Cubes of Two Numbers

--- Week 1/Lecture 2/Callbacks.js
+++ Week 1/Lecture 2/Callbacks.js
@@ -2,6 +2,10 @@ function square(a) {
     return a * a;
 }
 
+function cube(a) {
+    return a * a * a;
+}
+
 function sumOfSomething(a, b, fn) {
     const val1 = fn(a);
     const val2 = fn(b);
@@ -9,4 +13,6 @@ function sumOfSomething(a, b, fn) {
 }
 
 let ans = sumOfSomething(2, 3, square);
+let ans2 = sumOfSomething(2, 3, cube);
 console.log(ans);
+console.log(ans2);
