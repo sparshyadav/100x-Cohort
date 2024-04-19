0ddb17362b687e5b567dcdd380782c3c309c6ed9
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-20 01:38:15 +0530

    Created a POST Route to add Information

--- Week 2/Lecture 2/Hospital/index.js
+++ Week 2/Lecture 2/Hospital/index.js
@@ -10,7 +10,32 @@ const users = [{
     }]
 }]
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
+    let johnKidney = users[0]["Kidneys"];
+    let totalKidneys = johnKidney.length;
+
+    let healthyKidney = 0;
+    johnKidney.filter((kid) => {
+        if (kid.healthy == true) {
+            healthyKidney++;
+        }
+    })
+
+    let unhealthyKidney = totalKidneys - healthyKidney;
+
+    res.send(`The Total Number of Kidneys is: ${totalKidneys}
+                The Total Number of Healthy Kidneys is: ${healthyKidney}
+                The Total Number of Unhealthy Kidneys is: ${unhealthyKidney}`);
+})
+
+app.post("/", (req, res)=>{
+    const healthy=req.body.healthy;
+    users[0].Kidneys.push({
+        healthy: healthy
+    })
+
     let johnKidney = users[0]["Kidneys"];
     let totalKidneys = johnKidney.length;
 
