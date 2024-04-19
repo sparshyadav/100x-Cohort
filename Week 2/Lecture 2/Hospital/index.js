fe47af3c7cfad996be99f64d7d156a41972b7a08
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-20 01:29:51 +0530

    Created a Basic GET Route to get Information

---
+++ Week 2/Lecture 2/Hospital/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const users = [{
+    name: "john",
+    Kidneys: [{
+        healthy: false
+    }, {
+        healthy: true
+    }]
+}]
+
+app.get("/", (req, res) => {
+    let johnKidney = users[0]["Kidneys"];
+    let totalKidneys = johnKidney.length;
+
+    let healthyKidney = 0;
+    johnKidney.filter((kid) => {
+        if (kid.healthy == true) {
+            healthyKidney++;
+        }
+    })
+
+    let unhealthyKidney = totalKidneys - healthyKidney;
+
+    res.send(`The Total Number of Kidneys is: ${totalKidneys}
+                The Total Number of Healthy Kidneys is: ${healthyKidney}
+                The Total Number of Unhealthy Kidneys is: ${unhealthyKidney}`);
+})
+
+app.listen(3000, () => {
+    console.log("Server Started at Port: 3000");
+})
