8a593011cc2dad9c827d9d34658f48fda14629bc
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-19 22:28:11 +0530

    Using BodyParser Middleware

--- Week 2/Lecture 2/index.js
+++ Week 2/Lecture 2/index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const bodyParser=require("body-parser")
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post("/conversations", (req, res)=>{
+  console.log(req.body);
+  res.send({
+    msg: "Message Read"
+  })
 })
 
 app.listen(port, () => {
