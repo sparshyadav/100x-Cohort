4a720dd83e6240c03e72f3e591f5ae324038b5ff
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-20 01:49:07 +0530

    Created a DELETE Route to Delete some Information

--- Week 2/Lecture 2/Hospital/index.js
+++ Week 2/Lecture 2/Hospital/index.js
@@ -75,6 +75,36 @@ app.put("/", (req, res) => {
                 The Total Number of Unhealthy Kidneys is: ${unhealthyKidney}`);
 })
 
+app.delete("/", (req, res) => {
+    const newKidneys = [];
+
+    users[0]["Kidneys"].map((kid) => {
+        if (kid.healthy = true) {
+            newKidneys.push({
+                healthy: true
+            })
+        }
+    })
+
+    users[0]["Kidneys"] = newKidneys;
+
+    let johnKidney = users[0]["Kidneys"];
+    let totalKidneys = johnKidney.length;
+
+    let healthyKidney = 0;
+    johnKidney.filter((kid) => {
+        if (kid.healthy == true) {
+            healthyKidney++;
+        }
+    })
+
+    let unhealthyKidney = totalKidneys - healthyKidney;
+
+    res.send(`The Total Number of Kidneys is: ${totalKidneys}
+                The Total Number of Healthy Kidneys is: ${healthyKidney}
+                The Total Number of Unhealthy Kidneys is: ${unhealthyKidney}`);
+})
+
 app.listen(3000, () => {
     console.log("Server Started at Port: 3000");
 })
