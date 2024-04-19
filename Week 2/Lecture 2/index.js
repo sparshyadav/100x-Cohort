702f8aae8af3744427a6e35a8d4e2d8d85191b12
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-20 01:15:04 +0530

    Taking Input from Query

--- Week 2/Lecture 2/index.js
+++ Week 2/Lecture 2/index.js
@@ -1,15 +1,28 @@
 const express = require('express')
-const bodyParser=require("body-parser")
+const bodyParser = require("body-parser")
 const app = express()
 const port = 3000
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+function calculateSum(n){
+  let ans=0;
+  for(let i=1; i<=n; i++){
+    ans+=i;
+  }
+
+  return ans;
+}
+
+app.get("/sum", (req, res) => {
+  // res.send('Hello World!')
+
+  const n=req.query.n;
+  const sum=calculateSum(n);
+  res.send("Hi your ans is: "+ sum);
 })
 
-app.post("/conversations", (req, res)=>{
+app.post("/conversations", (req, res) => {
   console.log(req.body);
   res.send({
     msg: "Message Read"
