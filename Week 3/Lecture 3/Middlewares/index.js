3a6780d33a58bfbed57c54ec50041375bd7116b6
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-21 09:34:38 +0530

    Using a Function instead of Middlewares

--- Week 3/Lecture 3/Middlewares/index.js
+++ Week 3/Lecture 3/Middlewares/index.js
@@ -4,6 +4,27 @@ const app = express();
 
 app.use(express.json());
 
+function isOldEnough(age) {
+    if (age >= 14) {
+        return true;
+    }
+
+    return false;
+}
+
+app.get("/ticket", (req, res) => {
+    if (isOldEnough(req.query.age)) {
+        res.json({
+            msg: "You have rode ride 1"
+        })
+    }
+    else {
+        res.status(401).json({
+            msg: "Your Age is not Enough to Ride ride 1"
+        })
+    }
+})
+
 app.listen(3000, () => {
     console.log("The Server is Running at PORT: 3000");
 })
