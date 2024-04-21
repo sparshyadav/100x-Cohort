93044c3bb3b0d7a972e3430555418131a8ab783d
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-04-21 22:04:06 +0530

    Updated Models

--- Blog Backend/index.js
+++ Blog Backend/index.js
@@ -1,9 +1,9 @@
 const express=require("express");
 const connectDatabase = require("./config/database");
+const blogRoutes=require("./routes/blog");
 require("dotenv").config();
 
 const PORT=process.env.PORT || 3000;
-
 const app=express();
 
 app.use(express.json());
@@ -11,6 +11,7 @@ app.use(express.json());
 app.get("/", (req, res)=>{
     res.send(`<h1>This is Homepage for Blog APP</h1>`)
 })
+
 
 connectDatabase();
 app.listen(PORT, ()=>{
